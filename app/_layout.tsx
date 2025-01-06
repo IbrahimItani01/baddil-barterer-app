a2a77d07570b74a01d21df123512a6d2f81654bd
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-06 03:35:42 +0200

    layouts<>handled StatusBar in app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,3 +4,8 @@ import { StatusBar, useColorScheme } from "react-native";
 export default function Layout() {
 	const colorScheme = useColorScheme();
 
+	return (
+		<>
+			<StatusBar
+				barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
+			/>
