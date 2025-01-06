c955e66fa46cce81642e6e7d07080217bccf3806
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-06 03:51:44 +0200

    components<>created a CustomText component to handle dark and light modes efficiently

---
+++ components/base/CustomText.tsx
@@ -0,0 +1,23 @@
+import { colors } from "@/lib/constants/colors.constant";
+import { CustomTextInterface } from "@/lib/interfaces/CustomText.interface";
+import React from "react";
+import { Text, useColorScheme } from "react-native";
+
+const CustomText = (prop: CustomTextInterface) => {
+	const theme = useColorScheme();
+	return (
+		<Text
+			style={{
+				color:
+					theme === "dark"
+						? `${colors["white-font"]}`
+						: `${colors["black-font"]}`,
+			}}
+			className={prop.NativeClasses}
+		>
+			{prop.content}
+		</Text>
+	);
+};
+
+export default CustomText;
