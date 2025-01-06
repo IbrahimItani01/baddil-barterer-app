70f41c624c8c659a960de128be893127d9423e83
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-06 23:26:08 +0200

    screens<>imported global.css file into home screen file

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import CustomText from "@/components/base/CustomText";
 import CustomView from "@/components/base/CustomView";
-
+import "../../global.css";
 export default function Tab() {
 	return (
 			<CustomView NativeClasses='h-full flex justify-center items-center'>
