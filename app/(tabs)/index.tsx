f47db32b82855de2b9c94818a4dfed57aaf52678
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-06 23:26:52 +0200

    screens<>returned temporary tsx to handle onboarding for testing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,18 @@
 import CustomText from "@/components/base/CustomText";
 import CustomView from "@/components/base/CustomView";
 import "../../global.css";
+import CustomLink from "@/components/base/CustomLink";
 export default function Tab() {
 	return (
-			<CustomView NativeClasses='h-full flex justify-center items-center'>
-				<CustomText content='home' />
-			</CustomView>
+		<CustomView
+			mainScreen={true}
+			NativeClasses='flex justify-center items-center'
+		>
+			<CustomText content='home' />
+			<CustomLink
+				href={'/onBoarding'}
+				content='onboard'
+			/>
+		</CustomView>
 	);
 }
