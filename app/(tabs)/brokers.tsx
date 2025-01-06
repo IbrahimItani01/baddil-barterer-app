da34c94e5e87a99d2c6e2bafbe4c045c8351be54
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-06 04:10:26 +0200

    screens<>created initial screen structure for the brokers screen

---
+++ app/(tabs)/brokers.tsx
@@ -0,0 +1,10 @@
+import CustomText from "@/components/base/CustomText";
+import CustomView from "@/components/base/CustomView";
+
+export default function Tab() {
+	return (
+			<CustomView NativeClasses='h-full flex justify-center items-center'>
+				<CustomText content='brokers' />
+			</CustomView>
+	);
+}
