aa5dd0b672552d7c3bdf974a1e70967beeb349f4
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-06 23:25:01 +0200

    layouts<>imported global.css file into layout file of tabs route group

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,6 +3,7 @@ import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useColorScheme } from "react-native";
 import { colors } from "@/lib/constants/colors.constant";
 import Loader from "@/components/base/Loader";
+import '../../global.css'
 
 export default function TabLayout() {
 	const theme = useColorScheme();
