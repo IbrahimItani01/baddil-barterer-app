1eaa89f36920d29a58594fade9e58e64e3827756
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-06 23:43:49 +0200

    layouts<>provided redux store in main app layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { StatusBar, useColorScheme } from "react-native";
 import "../global.css";
+import { Provider } from "react-redux";
+import store from "@/store/store";
 import AppNavigator from "@/components/AppNavigator";
 
 export default function Layout() {
 	const colorScheme = useColorScheme();
 
 	return (
-		<>
+		<Provider store={store}>
 			<StatusBar
 				barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
 			/>
 			<AppNavigator />
+		</Provider>
 	);
 }
