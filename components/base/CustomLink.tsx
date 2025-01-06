340c317e603f238f1011d87b1f5bf1f7bf13c753
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-06 03:49:28 +0200

    components<>declared handlePress function in CustomLink component to handel dynamic navigation actions based on props passed

--- components/base/CustomLink.tsx
+++ components/base/CustomLink.tsx
@@ -2,3 +2,13 @@ import { Link, router } from "expo-router";
 import { CustomLinkInterface } from "@/lib/interfaces/CustomLink.interface";
 
 const CustomLink = (prop: CustomLinkInterface) => {
+  const handlePress = () => {
+    if (prop.href === "back") {
+      if (prop.behavior === "replace") {
+        router.replace("/");
+      } else {
+        router.back();
+      }
+    }
+  };
+
