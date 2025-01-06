15fe8d3dc0a7b94fa8b108f609b8ab3423aa2010
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-06 04:02:46 +0200

    layouts<>imported necessary components in tabs layout for bottom navbar

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,6 @@
+import { Tabs } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { useColorScheme } from "react-native";
+import { colors } from "@/lib/constants/colors.constant";
+import Loader from "@/components/base/Loader";
+
