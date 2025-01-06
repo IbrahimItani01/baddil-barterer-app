8f288c2f9d311146dd383f84a6b338bd994d6576
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-06 23:43:07 +0200

    slices<>declared userSlice reducers to handle onboarding state

--- store/slices/user.slice.ts
+++ store/slices/user.slice.ts
@@ -9,3 +9,23 @@ const initialState: UserState = {
 	isLoggedIn: false,
 	hasOnboarded: false,
 };
+
+const userSlice = createSlice({
+	name: "user",
+	initialState,
+	reducers: {
+		login(state) {
+			state.isLoggedIn = true;
+		},
+		logout(state) {
+			state.isLoggedIn = false;
+		},
+		completeOnboarding(state) {
+			state.hasOnboarded = true;
+		},
+		setOnboarding(state, action) {
+			state.hasOnboarded = action.payload; // Set onboarding status dynamically
+		},
+	},
+});
+
