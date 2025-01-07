341004ed6a145dd6318c7ee7883c924506d764e7
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 20:46:37 +0200

    components<>imported necessary packages and components to use in CustomAuthINput component

---
+++ components/base/CustomAuthInput.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Input } from "@rneui/themed";
+import { AuthInputInterface } from "@/lib/interfaces/auth.interface";
+import { Text, TouchableOpacity, useColorScheme, View } from "react-native";
+
