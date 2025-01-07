e2eb9166f6a732681123316d3f39ccf53ffc581a
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 20:50:00 +0200

    index<>declared interface for the Submit data

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -3,3 +3,10 @@ import "../../global.css";
 import Login from "@/components/auth/Login";
 import Register from "@/components/auth/Register";
 
+export interface SubmitDTO {
+	username?: string;
+	email: string;
+	password: string;
+	confirmPassword?: string;
+}
+
