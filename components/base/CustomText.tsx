bffc70bf2a23e90e20bb35b0fe855bb6d283f6e3
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 20:44:14 +0200

    components<>imported global.css file

--- components/base/CustomText.tsx
+++ components/base/CustomText.tsx
@@ -2,7 +2,7 @@ import { colors } from "@/lib/constants/colors.constant";
 import { CustomTextInterface } from "@/lib/interfaces/CustomText.interface";
 import React from "react";
 import { Text, useColorScheme } from "react-native";
-
+import "../../global.css";
 const CustomText = (prop: CustomTextInterface) => {
 	const theme = useColorScheme();
 	return (
