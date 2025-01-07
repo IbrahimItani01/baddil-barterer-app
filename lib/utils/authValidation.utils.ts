4a7b4e79fb4f5fd7b0f264854424828f6d622021
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 22:04:44 +0200

    utils<>added isFormValid util function

--- lib/utils/authValidation.utils.ts
+++ lib/utils/authValidation.utils.ts
@@ -1,3 +1,5 @@
+import { AuthFormState } from "@/store/slices/auth.slice";
+
 // sets validation for email
 export const isValidEmail = (email: string): boolean => {
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -9,4 +11,10 @@ export const isValidPassword = (password: string): boolean => {
 	const passwordRegex =
 		/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{12,}$/;
 	return passwordRegex.test(password);
+};
+
+export const isFormValid = (state: AuthFormState) => {
+	return (
+		!state.emailError && !state.passwordError && !state.confirmPasswordError
+	);
 };
