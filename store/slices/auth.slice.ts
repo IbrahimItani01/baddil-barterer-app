19b7702a71f73efd6a97df117a67b984a2284699
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 22:00:44 +0200

    slices<>declared AuthFormState

--- store/slices/auth.slice.ts
+++ store/slices/auth.slice.ts
@@ -3,6 +3,15 @@ import {
 	isValidPassword,
 } from "@/lib/utils/authValidation.utils";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+const initialState: AuthFormState = {
+	username: "",
+	email: "",
+	password: "",
+	confirmPassword: "",
+	emailError: "",
+	passwordError: "",
+	confirmPasswordError: "",
+};
 
 const authSlice = createSlice({
 });
