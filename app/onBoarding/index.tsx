3ac4fc1797902411811bb9ea9028d9b25c82e013
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-08 01:10:35 +0200

    index<>re-located DoneButton to external file in onBoarding screen

--- app/onBoarding/index.tsx
+++ app/onBoarding/index.tsx
@@ -1,34 +1,18 @@
 import React from "react";
 import Onboarding from "react-native-onboarding-swiper";
-import { Image, TouchableOpacity, useColorScheme } from "react-native";
+import { Image, useColorScheme } from "react-native";
 import { colors } from "@/lib/constants/colors.constant";
 import Loader from "@/components/base/Loader";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useDispatch } from "react-redux";
 import "../../global.css";
 import { completeOnboarding } from "@/store/slices/user.slice";
 import { router } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import DoneButton from "@/components/onBoarding/DoneButton";
 
 const OnboardingScreen = () => {
 	const theme = useColorScheme();
 	const dispatch = useDispatch();
-
-	const DoneButton = () => (
-		<TouchableOpacity
-			onPress={() => {
-				dispatch(completeOnboarding());
-				router.replace("/auth");
-			}}
-		>
-			<MaterialIcons
-				name='login'
-				size={35}
-				color={colors.primary}
-				style={{ marginRight: 20, marginVertical: 10 }}
-			/>
-		</TouchableOpacity>
-	);
 
 	const handleNav = async () => {
 		try {
