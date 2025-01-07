f58fd82c52f12620c63e4e639ba9eb724adde558
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 22:01:10 +0200

    slices<>exported AuthFormState interface

--- store/slices/auth.slice.ts
+++ store/slices/auth.slice.ts
@@ -3,6 +3,17 @@ import {
 	isValidPassword,
 } from "@/lib/utils/authValidation.utils";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface AuthFormState {
+	username: string;
+	email: string;
+	password: string;
+	confirmPassword: string;
+	emailError: string;
+	passwordError: string;
+	confirmPasswordError: string;
+}
+
 const initialState: AuthFormState = {
 	username: "",
 	email: "",
