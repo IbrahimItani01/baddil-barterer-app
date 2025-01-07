564946564a2d77a267578f14b2bf06e60a1a02f1
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 21:09:37 +0200

    components<>returned login component structure

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -53,3 +53,120 @@ const Login = ({ onPress, onSubmit }: AuthInterface) => {
 		}
 	};
 
+	return (
+		<CustomView mainScreen>
+			<SafeAreaView
+				style={{
+					flex: 1,
+					marginTop: 40,
+					flexDirection: "column",
+					gap: 50,
+				}}
+			>
+				<CustomView NativeClasses='flex items-center'>
+					<Logo />
+				</CustomView>
+				<CustomView NativeClasses='gap-10'>
+					<CustomView NativeClasses='ml-5 gap-2'>
+						<CustomText
+							content='Login'
+							NativeClasses='font-raleway-bold font-bold text-5xl'
+						/>
+						<CustomText
+							NativeClasses='font-nunito-semibold font-semibold'
+							content='Welcome back, login to continue!'
+						/>
+					</CustomView>
+					<CustomView NativeClasses='flex flex-col gap-2'>
+						<CustomAuthInput
+							label='Email'
+							placeholder='john@example.com'
+							type='email'
+							value={email}
+							onChangeText={setEmail}
+							errorMessage={emailError}
+						/>
+						<CustomAuthInput
+							label='Password'
+							placeholder='*******************'
+							type='password'
+							isPassword={true}
+							value={password}
+							onChangeText={setPassword}
+							errorMessage={passwordError}
+						/>
+					</CustomView>
+				</CustomView>
+				<CustomView NativeClasses='flex-1 flex-col justify-center'>
+					<Button
+						title='Login'
+						onPress={handleLogin}
+					/>
+					<Divider
+						style={{
+							marginVertical: 20,
+							marginHorizontal: 50,
+						}}
+						insetType='left'
+						width={1}
+					/>
+					<TouchableOpacity>
+						<View
+							style={{
+								borderColor: `${colors.primary}`,
+								borderWidth: 2,
+								padding: 10,
+								borderRadius: 50,
+								alignSelf: "center", // Correct centering for RN
+							}}
+						>
+							<FontAwesome5
+								name='google'
+								size={30}
+								color={colors.primary}
+							/>
+						</View>
+					</TouchableOpacity>
+				</CustomView>
+				<View
+					style={{
+						marginHorizontal: "auto",
+						display: "flex",
+						flexDirection: "row",
+						alignItems: "center",
+						gap: 5,
+					}}
+				>
+					<Loader>
+						<Text
+							style={{
+								color:
+									theme === "dark"
+										? `${colors["white-font"]}`
+										: `${colors["black-font"]}`,
+								fontFamily: "NunitoSans-Light",
+							}}
+						>
+							Don't have an account?
+						</Text>
+						<TouchableOpacity
+							className='flex items-center'
+							onPress={onPress}
+						>
+							<Text
+								style={{
+									textDecorationLine: "underline",
+								}}
+								className='text-primary font-nunito-semibold font-semibold'
+							>
+								Register
+							</Text>
+						</TouchableOpacity>
+					</Loader>
+				</View>
+			</SafeAreaView>
+		</CustomView>
+	);
+};
+
+export default Login;
