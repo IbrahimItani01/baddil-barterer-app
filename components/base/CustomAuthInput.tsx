d39add992ff476b838ae5e3802cc2a8606b95119
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 20:47:11 +0200

    components<>declared theme hook and function to handle forgot password in CustomAuthINput component

--- components/base/CustomAuthInput.tsx
+++ components/base/CustomAuthInput.tsx
@@ -3,3 +3,9 @@ import { Input } from "@rneui/themed";
 import { AuthInputInterface } from "@/lib/interfaces/auth.interface";
 import { Text, TouchableOpacity, useColorScheme, View } from "react-native";
 
+const CustomAuthInput = (props: AuthInputInterface) => {
+	const theme = useColorScheme();
+	const handleForgotPassword = () => {
+		// handle forgot password
+	};
+
