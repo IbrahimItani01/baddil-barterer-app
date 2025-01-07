69848e9c08f48f0e31ae664abd58099c98988759
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-08 01:17:46 +0200

    components<>added prop to handle style props passed in CustomView component

--- components/base/CustomView.tsx
+++ components/base/CustomView.tsx
@@ -3,16 +3,20 @@ import React from "react";
 import { CustomViewInterface } from "@/lib/interfaces/CustomView.interface";
 import { colors } from "@/lib/constants/colors.constant";
 import "../../global.css";
+
 const CustomView = (prop: CustomViewInterface) => {
 	const theme = useColorScheme();
 	return (
 		<View
 			className={prop.NativeClasses}
-			style={{
-				backgroundColor:
-					theme === "dark" ? `${colors["dark-bg"]}` : `${colors["light-bg"]}`,
-				height: prop.mainScreen ? "100%" : "auto",
-			}}
+			style={[
+				{
+					backgroundColor:
+						theme === "dark" ? `${colors["dark-bg"]}` : `${colors["light-bg"]}`,
+					height: prop.mainScreen ? "100%" : "auto",
+				},
+				prop.style,
+			]}
 		>
 			{prop.children}
 		</View>
