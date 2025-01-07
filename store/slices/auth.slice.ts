fa06cd7d1a1f794ecb13af248e093b755d929caf
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 21:58:14 +0200

    slices<>declared authSlice to handel auth logic and states

---
+++ store/slices/auth.slice.ts
@@ -0,0 +1,8 @@
+import {
+	isValidEmail,
+	isValidPassword,
+} from "@/lib/utils/authValidation.utils";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+});
