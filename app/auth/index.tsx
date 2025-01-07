053811c8ab7caa9159a05d38dae1b14e7a6d8363
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-08 01:09:16 +0200

    index<>removed changeForm logic in main auth file

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -1,19 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "../../global.css";
 
 import Login from "@/components/auth/Login";
 import Register from "@/components/auth/Register";
-import { SubmitDTO } from "@/lib/interfaces/auth.interface";
+import { SubmitDTO } from "@/lib/interfaces/auth/auth.interface";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { setIsLogin } from "@/store/slices/auth.slice";
 
 const Index = () => {
-	const dispatch = useAppDispatch();
 	const isLogin = useAppSelector((state) => state.auth.isLogin);
 
-	const changeForm = () => {
-		dispatch(setIsLogin(!isLogin));
-	};
+	
 
 	const handleSubmit = async (params: SubmitDTO) => {
 		// Handle calling API depending on isLogin state
@@ -23,12 +19,10 @@ const Index = () => {
 		<>
 			{isLogin ? (
 				<Login
-					onPress={changeForm}
 					onSubmit={handleSubmit}
 				/>
 			) : (
 				<Register
-					onPress={changeForm}
 					onSubmit={handleSubmit}
 				/>
 			)}
