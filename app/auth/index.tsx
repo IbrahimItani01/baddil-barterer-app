ef8780f911dc56c1b97757cf70ffb8adc8390366
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 22:45:32 +0200

    screens<>re-formed the use of isLogin state in the index file of auth screen

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -1,29 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import "../../global.css";
 
 import Login from "@/components/auth/Login";
 import Register from "@/components/auth/Register";
 import { SubmitDTO } from "@/lib/interfaces/auth.interface";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { setIsLogin } from "@/store/slices/auth.slice";
 
 const Index = () => {
-	const [isLogin, setIsLogin] = useState(true);
-	const changeFrom = () => {
-		setIsLogin(!isLogin);
+	const dispatch = useAppDispatch();
+	const isLogin = useAppSelector((state) => state.auth.isLogin);
+
+	const changeForm = () => {
+		dispatch(setIsLogin(!isLogin));
 	};
+
 	const handleSubmit = async (params: SubmitDTO) => {
-		// TODO: handle calling api depending on isLogin state
+		// Handle calling API depending on isLogin state
 	};
 
 	return (
 		<>
 			{isLogin ? (
 				<Login
-					onPress={changeFrom}
+					onPress={changeForm}
 					onSubmit={handleSubmit}
 				/>
 			) : (
 				<Register
-					onPress={changeFrom}
+					onPress={changeForm}
 					onSubmit={handleSubmit}
 				/>
 			)}
