574ef74b937b2c1920f627517af7067fdd1c7a24
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 20:39:29 +0200

    interfaces<>exported AuthInputInterface for the custom AuthInput component

--- lib/interfaces/auth.interface.ts
+++ lib/interfaces/auth.interface.ts
@@ -4,3 +4,14 @@ export interface AuthInterface {
 	onPress: () => void;
 	onSubmit: (params: SubmitDTO) => void;
 }
+export interface AuthInputInterface {
+	placeholder: string;
+	isPassword?: boolean;
+	type?: "email" | "password" | "username";
+	NativeClasses?: string;
+	errorMessage?: string;
+	label: string;
+	onChangeText?: (text: string) => void; 
+	value?: string; 
+
+}
