69cabe31c1a7e574867ac2d88e6d9c0321ba0a32
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-07 20:41:55 +0200

    utils<>declared password validation function

--- lib/utils/authValidation.utils.ts
+++ lib/utils/authValidation.utils.ts
@@ -4,3 +4,9 @@ export const isValidEmail = (email: string): boolean => {
 	return emailRegex.test(email);
 };
 
+// sets requirements for password
+export const isValidPassword = (password: string): boolean => {
+	const passwordRegex =
+		/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{12,}$/;
+	return passwordRegex.test(password);
+};
