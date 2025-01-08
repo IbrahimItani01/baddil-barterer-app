95f636533dcb1d0491f4f2a90f8ca88549a6cf0e
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-09 00:38:38 +0200

    layouts<>handled to clear async storage every time the app reloads in dev mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StatusBar, useColorScheme } from "react-native";
 import "../global.css";
 import { Provider } from "react-redux";
@@ -6,6 +6,18 @@ import store from "@/store/store";
 import AppNavigator from "@/components/AppNavigator";
 import Loader from "@/components/base/Loader";
 import ScreenLoader from "@/components/base/ScreenLoader";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const clearStorageOnDev = async () => {
+	if (__DEV__) {
+		try {
+			await AsyncStorage.clear();
+			console.log("AsyncStorage cleared in development mode");
+		} catch (error) {
+			console.error("Failed to clear AsyncStorage", error);
+		}
+	}
+};
 
 export default function Layout() {
 	const colorScheme = useColorScheme();
