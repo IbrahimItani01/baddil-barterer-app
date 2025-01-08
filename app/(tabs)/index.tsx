23e59bfefd73117c142c54ce90bcd030f46bdd1b
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-09 00:40:49 +0200

    index<>applied temporary logic to return to onboarding screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,26 @@
 import CustomText from "@/components/base/CustomText";
 import CustomView from "@/components/base/CustomView";
 import "../../global.css";
-import CustomLink from "@/components/base/CustomLink";
+import { router } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useDispatch } from "react-redux";
+import { logout } from "@/store/slices/user.slice";
+import { Text } from "@rneui/themed";
+
 export default function Tab() {
+	const dispatch = useDispatch();
+	const handlePress = async () => {
+		await AsyncStorage.clear();
+		dispatch(logout());
+		router.replace("/onBoarding");
+	};
 	return (
 		<CustomView
 			mainScreen={true}
 			NativeClasses='flex justify-center items-center'
 		>
 			<CustomText content='home' />
-			<CustomLink
-				href={'/onBoarding'}
-				content='onboard'
-			/>
+			<Text onPress={handlePress}>Reset</Text>
 		</CustomView>
 	);
 }
