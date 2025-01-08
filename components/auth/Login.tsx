be174dc34c12327552e27733a363f2083492f7e1
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-08 02:19:05 +0200

    components<>handled styles in the outer layer for custom components

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -22,22 +22,58 @@ const Login = ({ onSubmit }: AuthInterface) => {
 	};
 
 	return (
-		<MainScreen>
+		<MainScreen
+			style={{
+				flex: 1,
+				marginTop: 40,
+				flexDirection: "column",
+				gap: 50,
+			}}
+		>
 			<Headline
+				logoStyle={{
+					display: "flex",
+					alignItems: "center",
+				}}
+				headlineStyle={{
+					marginLeft: 15,
+					gap: 10,
+				}}
 				title='Login'
 				description='Welcome back, login to continue!'
 			/>
-			<AuthFields type='login' />
+			<AuthFields
+				containerStyle={{
+					gap: 10,
+				}}
+				type='login'
+			/>
 			<AuthActions
 				type='login'
 				buttonContent='Login'
 				onSubmit={handleLogin}
 				// TODO: add google auth
 				handleGoogleAuth={() => {}}
+				containerStyle={{
+					flex: 1,
+					flexDirection: "column",
+					justifyContent: "center",
+				}}
+				dividerStyle={{
+					marginVertical: 20,
+					marginHorizontal: 50,
+				}}
 			/>
 			<AuthFooter
 				hyperLinkContent='Register'
 				hintContent="Don't have an account?"
+				containerStyle={{
+					marginHorizontal: "auto",
+					flexDirection: "row",
+					alignItems: "center",
+					gap: 5,
+					marginBottom: 10,
+				}}
 			/>
 		</MainScreen>
 	);
