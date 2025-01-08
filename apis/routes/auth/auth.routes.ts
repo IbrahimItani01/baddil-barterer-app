81f472f9c05ca0d8a7a85aba57e7a44850b09859
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-09 00:37:13 +0200

    routes<>passed dispatch parameter in login function

--- apis/routes/auth/auth.routes.ts
+++ apis/routes/auth/auth.routes.ts
@@ -1,8 +1,15 @@
 import { APIS_BASE_URL } from "@/apis/main";
+import { login, setUserName } from "@/store/slices/user.slice";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+import { router } from "expo-router";
 import { Alert } from "react-native";
 
-export const loginUser = async (email: string, password: string) => {
+export const loginUser = async (
+	dispatch: any,
+	email: string,
+	password: string
+) => {
 	try {
 		const response = await axios.post(`${APIS_BASE_URL}/auth/login`, {
 			emailOrIdToken: email,
