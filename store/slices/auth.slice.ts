c523082f287e8f571cf0352908e896642cf47246
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-09 00:41:47 +0200

    slices<>re-located validate form reducer to become external util function

--- store/slices/auth.slice.ts
+++ store/slices/auth.slice.ts
@@ -1,18 +1,11 @@
-import {
-	isValidEmail,
-	isValidPassword,
-	isValidUsername,
-	passwordsMatch,
-} from "@/lib/utils/authValidation.utils";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Alert } from "react-native";
 
 export interface AuthFormState {
-	username: string;
+	username?: string;
 	email: string;
 	password: string;
-	confirmPassword: string;
-	isLogin: boolean;
+	confirmPassword?: string;
+	isLogin?: boolean;
 }
 
 const initialState: AuthFormState = {
@@ -42,48 +35,6 @@ const authSlice = createSlice({
 		setIsLogin(state, action: PayloadAction<boolean>) {
 			state.isLogin = action.payload;
 		},
-		validateForm(state) {
-			const emailValid = isValidEmail(state.email);
-			const passwordValid = isValidPassword(state.password);
-			const usernameValid = isValidUsername(state.username);
-			const isPasswordsMatch = passwordsMatch(
-				state.password,
-				state.confirmPassword
-			);
-
-			if (state.isLogin) {
-				if (!emailValid && !passwordValid) {
-					Alert.alert("Invalid email and password!");
-				} else if (!emailValid) {
-					Alert.alert(
-						"Invalid email format!",
-						"Please enter a valid email address."
-					);
-				} else if (!passwordValid) {
-					Alert.alert(
-						"Invalid password format!",
-						"Password must be at least 12 characters, contain one uppercase letter, one number, and one special character."
-					);
-				}
-			} else {
-				const invalidFields = [];
-
-				if (!usernameValid) invalidFields.push("username");
-				if (!emailValid) invalidFields.push("email");
-				if (!passwordValid) invalidFields.push("password");
-				if (!isPasswordsMatch) invalidFields.push("confirm password");
-
-				if (invalidFields.length > 1) {
-					Alert.alert(
-						"Multiple invalid fields!",
-						`Please fix the following: ${invalidFields.join(", ")}.`
-					);
-				} else if (invalidFields.length === 1) {
-					const field = invalidFields[0];
-					Alert.alert(`Invalid ${field}!`, `Please enter a valid ${field}.`);
-				}
-			}
-		},
 
 		resetForm(state) {
 			state.username = "";
@@ -99,7 +50,6 @@ export const {
 	setEmail,
 	setPassword,
 	setConfirmPassword,
-	validateForm,
 	resetForm,
 	setIsLogin,
 } = authSlice.actions;
