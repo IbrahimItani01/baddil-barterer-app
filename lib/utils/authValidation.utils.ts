6c88239febc5f5b08f4d79f7c0acebd66c210ab4
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-09 01:13:58 +0200

    utils<>re-located the validateForm function in a util file

--- lib/utils/authValidation.utils.ts
+++ lib/utils/authValidation.utils.ts
@@ -17,3 +17,44 @@ export const isValidUsername = (username: string) => username.trim().length > 0;
 
 export const passwordsMatch = (password: string, confirmPassword: string) =>
 	password === confirmPassword;
+
+export const validateForm = (state: AuthFormState) => {
+	const emailValid = isValidEmail(state.email || "");
+	const passwordValid = isValidPassword(state.password || "");
+	const usernameValid = state.username ? isValidUsername(state.username) : true;
+	const isPasswordsMatch =
+		state.password && state.confirmPassword
+			? passwordsMatch(state.password, state.confirmPassword)
+			: true; // Assume match if not provided
+
+	let validationPayload = {
+		isValid: true,
+		errors: [] as string[],
+	};
+
+	if (state.isLogin) {
+		if (!emailValid && !passwordValid) {
+			validationPayload.isValid = false;
+			validationPayload.errors.push("Invalid email and password");
+		} else if (!emailValid) {
+			validationPayload.isValid = false;
+			validationPayload.errors.push("Invalid email format");
+		} else if (!passwordValid) {
+			validationPayload.isValid = false;
+			validationPayload.errors.push(
+				"Password must be at least 12 characters, contain one uppercase letter, one number, and one special character."
+			);
+		}
+	} else {
+		if (!usernameValid) validationPayload.errors.push("username");
+		if (!emailValid) validationPayload.errors.push("email");
+		if (!passwordValid) validationPayload.errors.push("password");
+		if (!isPasswordsMatch) validationPayload.errors.push("confirm password");
+
+		if (validationPayload.errors.length > 0) {
+			validationPayload.isValid = false;
+		}
+	}
+
+	return validationPayload;
+};
