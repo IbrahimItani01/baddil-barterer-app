57db50a4acd6690b9f70c28c364ca8c21593faa8
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-08 02:18:30 +0200

    index<>cleaned code in auth index file

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -4,12 +4,10 @@ import "../../global.css";
 import Login from "@/components/auth/Login";
 import Register from "@/components/auth/Register";
 import { SubmitDTO } from "@/lib/interfaces/auth/auth.interface";
-import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import {useAppSelector } from "@/store/hooks";
 
 const Index = () => {
 	const isLogin = useAppSelector((state) => state.auth.isLogin);
-
-	
 
 	const handleSubmit = async (params: SubmitDTO) => {
 		// Handle calling API depending on isLogin state
