b59afe50f48bc55957e8e7a10fa2d3ef94d55fbf
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-08 02:19:44 +0200

    components<>created styles related props to enable passing styles for component

--- components/auth/base/AuthFields.tsx
+++ components/auth/base/AuthFields.tsx
@@ -2,50 +2,64 @@ import React from "react";
 import CustomView from "@/components/base/CustomView";
 import CustomAuthInput from "@/components/base/CustomAuthInput";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
-import { setEmail, setPassword } from "@/store/slices/auth.slice";
+import {
+	setConfirmPassword,
+	setEmail,
+	setPassword,
+	setUsername,
+} from "@/store/slices/auth.slice";
+import { StyleProp, ViewStyle } from "react-native";
 
 interface AuthFieldsInterface {
 	type: "login" | "register";
+	containerStyle: StyleProp<ViewStyle>;
 }
 
 const AuthFields = (props: AuthFieldsInterface) => {
-	const { email, password } = useAppSelector((state) => state.auth);
+	const { username, email, password, confirmPassword } = useAppSelector(
+		(state) => state.auth
+	);
 	const dispatch = useAppDispatch();
 
 	return (
-		<CustomView
-			style={{
-				gap: 10,
-			}}
-		>
-			<CustomView
-				style={{
-					display: "flex",
-					flexDirection: "column",
-					gap: 2,
-				}}
-			>
-				{props.type === "register" && <>{/* HANDLE THE USERNAME INPUT */}</>}
+		<CustomView style={props.containerStyle}>
+			{props.type === "register" && (
 				<CustomAuthInput
-					label='Email'
-					placeholder='john@example.com'
-					type='email'
-					value={email}
-					onChangeText={(text) => dispatch(setEmail(text))}
+					label='Name'
+					placeholder='John Doe'
+					type='username'
+					value={username}
+					onChangeText={(text) => dispatch(setUsername(text))}
 				/>
+			)}
+			<CustomAuthInput
+				label='Email'
+				placeholder='john@example.com'
+				type='email'
+				value={email}
+				onChangeText={(text) => dispatch(setEmail(text))}
+			/>
 
+			<CustomAuthInput
+				label='Password'
+				placeholder='*******************'
+				type='password'
+				isPassword={true}
+				value={password}
+				forRegister={props.type === "register" ? true : false}
+				onChangeText={(text) => dispatch(setPassword(text))}
+			/>
+			{props.type === "register" && (
 				<CustomAuthInput
-					label='Password'
+					label='Confirm Password'
 					placeholder='*******************'
 					type='password'
 					isPassword={true}
-					value={password}
-					onChangeText={(text) => dispatch(setPassword(text))}
+					value={confirmPassword}
+					forRegister={true}
+					onChangeText={(text) => dispatch(setConfirmPassword(text))}
 				/>
-				{props.type === "register" && (
-					<>{/* HANDLE THE CONFIRM PASSWORD INPUT */}</>
-				)}
-			</CustomView>
+			)}
 		</CustomView>
 	);
 };
