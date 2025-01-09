c74006873f2017f50348544fa4731163c250d645
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-09 20:39:26 +0200

    components<>declared tierProgressBar component

---
+++ components/tabs/home/TierProgressBar.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const TierProgressBar = () => {
+	return (
+		<View>
+			<Text>TierProgressBar</Text>
+		</View>
+	);
+};
+
+export default TierProgressBar;
