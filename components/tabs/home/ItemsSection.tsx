79a5d8049bb55d2d04d37b123aa500c2a17264f7
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-09 20:39:45 +0200

    components<>declared ItemsSection component

---
+++ components/tabs/home/ItemsSection.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ItemsSection = () => {
+	return (
+		<View>
+			<Text>ItemsSection</Text>
+		</View>
+	);
+};
+
+export default ItemsSection;
