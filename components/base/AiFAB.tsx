828337ba60df408cde177f27a0aa289c306fcdaa
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-09 20:39:51 +0200

    components<>declared AiFAB component

---
+++ components/base/AiFAB.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AiFAB = () => {
+	return (
+		<View>
+			<Text>AiFAB</Text>
+		</View>
+	);
+};
+
+export default AiFAB;
