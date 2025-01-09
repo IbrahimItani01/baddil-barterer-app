2fe77f9c9aa5eae48e2b2e397975e9e19a522ea8
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-09 21:11:56 +0200

    routes<>declared an async function to fetch user profile picture

--- apis/routes/user/user.routes.ts
+++ apis/routes/user/user.routes.ts
@@ -1,4 +1,5 @@
 import { APIS_BASE_URL } from "@/apis/main";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 export const checkUserByEmail = async (email: string): Promise<boolean> => {
@@ -11,4 +12,20 @@ export const checkUserByEmail = async (email: string): Promise<boolean> => {
 			console.error(error);
 			return false;
 		});
+};
+export const serveUserProfileImage = async () => {
+	const token = await AsyncStorage.getItem("jwtToken");
+	if (!token) return;
+	return await axios
+		.get(`${APIS_BASE_URL}/users/profile-picture`, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		})
+		.then((response) => {
+			if (response.data.success) {
+				return response.data.data;
+			}
+		})
+		.catch((e) => false);
 };
