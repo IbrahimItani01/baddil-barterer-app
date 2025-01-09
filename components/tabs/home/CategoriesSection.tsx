b93b6520367cf4950b578ca00728b2aec3ca7e79
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-09 20:40:06 +0200

    components<>declared Categories component

---
+++ components/tabs/home/CategoriesSection.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CategoriesSection = () => {
+	return (
+		<View>
+			<Text>CategoriesSection</Text>
+		</View>
+	);
+};
+
+export default CategoriesSection;
