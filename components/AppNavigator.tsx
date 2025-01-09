4467528b22d23fec086da862fa7d30cbaa250473
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-09 20:43:27 +0200

    navigator<>cleaned code

--- components/AppNavigator.tsx
+++ components/AppNavigator.tsx
@@ -3,7 +3,7 @@ import { Stack, useRouter } from "expo-router";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { setOnboarding } from "@/store/slices/user.slice";
+import { login, logout, setOnboarding } from "@/store/slices/user.slice";
 
 const AppNavigator = () => {
 	const router = useRouter();
@@ -27,20 +27,18 @@ const AppNavigator = () => {
 			dispatch(logout());
 		}
 	};
+
+	// Check onboarding status from AsyncStorage
+	useEffect(() => {
 		checkOnboardingStatus();
+		checkLoginStatus();
 	}, [dispatch]);
 
 	// Handle navigation based on login and onboarding status
 	useEffect(() => {
-		if (!isLoggedIn) {
-			if (!hasOnboarded) {
-				router.replace("/onBoarding");
-			} else {
-				router.replace("/auth");
-			}
-		} else {
-			router.replace("/(tabs)");
-		}
+		if (!isLoggedIn && !hasOnboarded) router.replace("/onBoarding");
+		else if (!isLoggedIn && hasOnboarded) router.replace("/auth");
+		else router.replace("/(tabs)");
 	}, [isLoggedIn, hasOnboarded, router]);
 
 	return (
