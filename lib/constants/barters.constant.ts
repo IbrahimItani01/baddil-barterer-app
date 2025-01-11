8abe7aed207e55427360a66a92194b9cd6e49212
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 08:14:44 +0200

    constants<>created a mock data for barters

---
+++ lib/constants/barters.constant.ts
@@ -0,0 +1,30 @@
+export const bartersData = [
+	{
+		id: "1",
+		title: "Guitar",
+		location: "Ashrafieh",
+		timestamp: "2 days ago",
+		favorite: true,
+	},
+	{
+		id: "2",
+		title: "Guitar",
+		location: "Ashrafieh",
+		timestamp: "2 days ago",
+		favorite: false,
+	},
+	{
+		id: "3",
+		title: "Guitar",
+		location: "Ashrafieh",
+		timestamp: "2 days ago",
+		favorite: true,
+	},
+	{
+		id: "4",
+		title: "Guitar",
+		location: "Ashrafieh",
+		timestamp: "2 days ago",
+		favorite: false,
+	},
+];
