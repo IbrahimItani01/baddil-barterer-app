0624d4f0c6aaa0194d0efc276a0d48fa97a3cbcf
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 08:17:48 +0200

    components<>rendered Item cards

--- components/tabs/home/ItemsSection.tsx
+++ components/tabs/home/ItemsSection.tsx
@@ -1,5 +1,10 @@
-import { View, Text } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 import React from "react";
+import { bartersData } from "@/lib/constants/barters.constant";
+import ItemCard from "./base/ItemCard";
+import CustomText from "@/components/base/CustomText";
+import { fontFamily } from "@/lib/constants/fonts.constant";
+
 export interface Barter {
 	id: string;
 	title: string;
@@ -9,6 +14,10 @@ export interface Barter {
 }
 
 const ItemsSection = () => {
+	const renderItem = ({ item }: { item: Barter }) => {
+		return <ItemCard item={item} />;
+	};
+
 	return (
 		<View>
 			<Text>ItemsSection</Text>
