72e319b5fa59f8a25215a4b201621cbd4635677c
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 05:50:28 +0200

    layouts<>cleaned the code of the tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,99 +4,85 @@ import { colors } from "@/lib/constants/colors.constant";
 import Loader from "@/components/base/Loader";
 import "../../global.css";
 import { useAppSelector } from "@/store/hooks";
+import { fontFamily } from "@/lib/constants/fonts.constant";
+
+const tabScreenOptions = (theme: "dark" | "light") => ({
+	tabBarActiveTintColor: "#E60000",
+	tabBarLabelStyle: {
+		fontSize: 12,
+		fontFamily: `${fontFamily.NunitoSans.Medium}`,
+	},
+	tabBarStyle: {
+		backgroundColor: theme === "dark" ? colors["dark-bg"] : colors["light-bg"],
+	},
+	headerShown: false,
+});
+
+const tabScreensConfig = [
+	{
+		name: "index",
+		title: "Home",
+		icon: "home",
+		accessibilityLabel: "Home",
+		iconSize: 30,
+	},
+	{
+		name: "brokers",
+		title: "Brokers",
+		icon: "handshake",
+		iconSize: 30,
+	},
+	{
+		name: "baddil",
+		title: "",
+		icon: "add-circle",
+		iconSize: 55,
+		iconStyle: {
+			width: 55,
+			height: 55,
+			marginBottom: 50,
+		},
+	},
+	{
+		name: "chats",
+		title: "Chats",
+		icon: "chat",
+		iconSize: 30,
+	},
+	{
+		name: "profile",
+		title: "Profile",
+		icon: "account-circle",
+		iconSize: 30,
+	},
+];
 
 export default function TabLayout() {
 	const theme = useAppSelector((state) => state.system.colorScheme);
+
 	return (
 		<Loader>
-			<Tabs
-				screenOptions={{
-					tabBarActiveTintColor: "#E60000",
-					tabBarLabelStyle: { fontSize: 12, fontFamily: "NunitoSans-Medium" },
-					tabBarStyle: {
-						backgroundColor:
-							theme === "dark"
-								? `${colors["dark-bg"]}`
-								: `${colors["light-bg"]}`,
-					},
-					headerShown: false,
-				}}
-			>
-				<Tabs.Screen
-					name='index'
-					options={{
-						title: "Home",
-						tabBarIcon: ({ color }) => (
-							<MaterialIcons
-								name={"home"}
-								color={color}
-								size={30}
-							/>
-						),
-						tabBarAccessibilityLabel: "Home",
-					}}
-				/>
-
-				<Tabs.Screen
-					name='brokers'
-					options={{
-						title: "Brokers",
-						tabBarIcon: ({ color, focused }) => (
-							<MaterialIcons
-								name={"handshake"}
-								color={color}
-								size={30}
-							/>
-						),
-					}}
-				/>
-
-				<Tabs.Screen
-					name='baddil'
-					options={{
-						title: "",
-						tabBarIcon: ({ color, focused }) => (
-							<MaterialIcons
-								name={"add-circle"}
-								color={color}
-								size={55}
-							/>
-						),
-						tabBarIconStyle: {
-							width: 55,
-							height: 55,
-							marginBottom: 50,
-						},
-					}}
-				/>
-
-				<Tabs.Screen
-					name='chats'
-					options={{
-						title: "Chats",
-						tabBarIcon: ({ color, focused }) => (
-							<MaterialIcons
-								name={"chat"}
-								color={color}
-								size={30}
-							/>
-						),
-					}}
-				/>
-
-				<Tabs.Screen
-					name='profile'
-					options={{
-						title: "Profile",
-						tabBarIcon: ({ color, focused }) => (
-							<MaterialIcons
-								name={"account-circle"}
-								color={color}
-								size={30}
-							/>
-						),
-					}}
-				/>
+			<Tabs screenOptions={tabScreenOptions(theme ?? "light")}>
+				{tabScreensConfig.map(
+					({ name, title, icon, iconSize, accessibilityLabel, iconStyle }) => (
+						<Tabs.Screen
+							key={name}
+							name={name}
+							options={{
+								title,
+								tabBarIcon: ({ color }) => (
+									<MaterialIcons
+										name={icon as keyof typeof MaterialIcons.glyphMap}
+										color={color}
+										size={iconSize}
+									/>
+								),
+								tabBarAccessibilityLabel: accessibilityLabel,
+								tabBarIconStyle: iconStyle,
+							}}
+						/>
+					)
+				)}
 			</Tabs>
 		</Loader>
 	);
