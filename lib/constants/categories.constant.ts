8bbcb3c6074b7b231202e6d888ee6dd6bbc57a54
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 04:10:21 +0200

    constants<>added static data for categories for design

---
+++ lib/constants/categories.constant.ts
@@ -0,0 +1,179 @@
+export const categories = [
+    {
+      id: 1,
+      name: "vehicles",
+      iconName: "car",
+      subcategories: [
+        {
+          id: 2,
+          name: "cars",
+          iconName: "car",
+        },
+        {
+          id: 3,
+          name: "motorcycles",
+          iconName: "motorcycle",
+        },
+        {
+          id: 4,
+          name: "trucks",
+          iconName: "truck",
+        },
+        {
+          id: 5,
+          name: "bicycles",
+          iconName: "bicycle",
+        },
+      ],
+    },
+    {
+      id: 6,
+      name: "electronics",
+      iconName: "tablet-alt",
+      subcategories: [
+        {
+          id: 7,
+          name: "phones",
+          iconName: "mobile-alt",
+        },
+        {
+          id: 8,
+          name: "laptops",
+          iconName: "laptop",
+        },
+        {
+          id: 9,
+          name: "televisions",
+          iconName: "tv",
+        },
+        {
+          id: 10,
+          name: "cameras",
+          iconName: "camera",
+        },
+      ],
+    },
+    {
+      id: 11,
+      name: "furniture",
+      iconName: "couch",
+      subcategories: [
+        {
+          id: 12,
+          name: "sofas",
+          iconName: "couch",
+        },
+        {
+          id: 13,
+          name: "tables",
+          iconName: "table",
+        },
+        {
+          id: 14,
+          name: "beds",
+          iconName: "bed",
+        },
+      ],
+    },
+    {
+      id: 15,
+      name: "instruments",
+      iconName: "music",
+      subcategories: [
+        {
+          id: 16,
+          name: "guitars",
+          iconName: "guitar",
+        },
+        {
+          id: 17,
+          name: "keyboards",
+          iconName: "keyboard",
+        },
+        {
+          id: 18,
+          name: "drums",
+          iconName: "drum",
+        },
+      ],
+    },
+    {
+      id: 19,
+      name: "fashion",
+      iconName: "tshirt",
+      subcategories: [
+        {
+          id: 20,
+          name: "clothing",
+          iconName: "shopping-bag",
+        },
+        {
+          id: 21,
+          name: "shoes",
+          iconName: "shoe-prints",
+        },
+        {
+          id: 22,
+          name: "accessories",
+          iconName: "watch",
+        },
+      ],
+    },
+    {
+      id: 23,
+      name: "books",
+      iconName: "book",
+      subcategories: [],
+    },
+    {
+      id: 24,
+      name: "sports",
+      iconName: "futbol",
+      subcategories: [
+        {
+          id: 25,
+          name: "gym equipment",
+          iconName: "dumbbell",
+        },
+        {
+          id: 26,
+          name: "bicycles",
+          iconName: "bicycle",
+        },
+        {
+          id: 27,
+          name: "outdoor gear",
+          iconName: "campground",
+        },
+      ],
+    },
+    {
+      id: 28,
+      name: "toys",
+      iconName: "puzzle-piece",
+      subcategories: [],
+    },
+    {
+      id: 29,
+      name: "appliances",
+      iconName: "blender",
+      subcategories: [
+        {
+          id: 30,
+          name: "fridges",
+          iconName: "snowflake",
+        },
+        {
+          id: 31,
+          name: "washing machines",
+          iconName: "soap",
+        },
+        {
+          id: 32,
+          name: "microwaves",
+          iconName: "hotdog",
+        },
+      ],
+    },
+  ];
+  
