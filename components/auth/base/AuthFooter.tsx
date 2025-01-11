66e79f176be0c1924e448780d056d40a5cd6724f
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 05:51:59 +0200

    components<>used stylesheets in authFooter

--- components/auth/base/AuthFooter.tsx
+++ components/auth/base/AuthFooter.tsx
@@ -1,57 +1,63 @@
 import {
 	View,
 	Text,
-	useColorScheme,
 	TouchableOpacity,
 	StyleProp,
 	ViewStyle,
+	StyleSheet,
 } from "react-native";
 import React from "react";
 import Loader from "@/components/base/Loader";
 import { colors } from "@/lib/constants/colors.constant";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { resetForm, setIsLogin } from "@/store/slices/auth.slice";
+import { fontFamily } from "@/lib/constants/fonts.constant";
 
 interface AuthFooterInterface {
 	hyperLinkContent: string;
 	hintContent: string;
 	containerStyle: StyleProp<ViewStyle>;
 }
+
 const AuthFooter = (props: AuthFooterInterface) => {
 	const dispatch = useAppDispatch();
 	const isLogin = useAppSelector((state) => state.auth.isLogin);
 	const theme = useAppSelector((state) => state.system.colorScheme);
+
 	const changeForm = () => {
 		dispatch(setIsLogin(!isLogin));
 		dispatch(resetForm());
 	};
+
 	return (
 		<View style={props.containerStyle}>
 			<Loader>
 				<Text
-					style={{
-						color:
-							theme === "dark"
-								? `${colors["white-font"]}`
-								: `${colors["black-font"]}`,
-						fontFamily: "NunitoSans-Light",
-					}}
+					style={[
+						styles.hintText,
+						{
+							color:
+								theme === "dark" ? colors["white-font"] : colors["black-font"],
+						},
+					]}
 				>
 					{props.hintContent}
 				</Text>
 				<TouchableOpacity onPress={changeForm}>
-					<Text
-						style={{
-							textDecorationLine: "underline",
-						}}
-						className='text-primary font-nunito-semibold font-semibold'
-					>
-						{props.hyperLinkContent}
-					</Text>
+					<Text style={styles.hyperLinkText}>{props.hyperLinkContent}</Text>
 				</TouchableOpacity>
 			</Loader>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	hintText: {
+		fontFamily: `${fontFamily.NunitoSans.Light}`,
+	},
+	hyperLinkText: {
+		textDecorationLine: "underline",
+	},
+});
 
 export default AuthFooter;
