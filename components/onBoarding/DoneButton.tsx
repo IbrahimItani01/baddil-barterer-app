8497aeaee37251583e99d98967a557d1042bdac1
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 05:52:56 +0200

    components<>used stylesheets in DoneButton

--- components/onBoarding/DoneButton.tsx
+++ components/onBoarding/DoneButton.tsx
@@ -1,33 +1,34 @@
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { completeOnboarding } from "@/store/slices/user.slice";
 import { router } from "expo-router";
 import { colors } from "@/lib/constants/colors.constant";
+import { fontFamily } from "@/lib/constants/fonts.constant";
 
 const DoneButton = () => {
 	const dispatch = useDispatch();
 
+	const handlePress = () => {
+		dispatch(completeOnboarding());
+		router.replace("/auth");
+	};
+
 	return (
-		<TouchableOpacity
-			onPress={() => {
-				dispatch(completeOnboarding());
-				router.replace("/auth");
-			}}
-		>
-			<Text
-				style={{
-					fontFamily: "NunitoSans-Bold",
-					color: colors.primary,
-					marginRight: 20,
-					marginVertical: 10,
-					fontSize: 16,
-				}}
-			>
-				Let's Go!
-			</Text>
+		<TouchableOpacity onPress={handlePress}>
+			<Text style={styles.buttonText}>Let's Go!</Text>
 		</TouchableOpacity>
 	);
 };
 
 export default DoneButton;
+
+const styles = StyleSheet.create({
+	buttonText: {
+		fontFamily: `${fontFamily.NunitoSans.Bold}`,
+		color: colors.primary,
+		marginRight: 20,
+		marginVertical: 10,
+		fontSize: 16,
+	},
+});
