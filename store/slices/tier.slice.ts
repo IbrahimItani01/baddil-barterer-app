12cfb92910b4116ed1033590912bd437d56952da
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 03:57:58 +0200

    slices<>declared reducers for tier slice

--- store/slices/tier.slice.ts
+++ store/slices/tier.slice.ts
@@ -14,3 +14,37 @@ const initialState: TierState = {
 	progress: 40,
 };
 
+const tierSlice = createSlice({
+	name: "tiers",
+	initialState,
+	reducers: {
+		setCurrentTier(state, action: PayloadAction<string>) {
+			state.currentTier = action.payload;
+		},
+		setTierRequirement(state, action: PayloadAction<number | null>) {
+			state.tierRequirement = action.payload;
+		},
+		setCompletedBarters(state, action: PayloadAction<number | null>) {
+			state.completedBarters = action.payload;
+		},
+		setProgress(state, action: PayloadAction<number | 0>) {
+			state.progress = action.payload;
+		},
+		resetTierState(state) {
+			state.currentTier = "";
+			state.tierRequirement = null;
+			state.completedBarters = null;
+			state.progress = 0;
+		},
+	},
+});
+
+export const {
+	setCurrentTier,
+	setTierRequirement,
+	setCompletedBarters,
+	setProgress,
+	resetTierState,
+} = tierSlice.actions;
+
+export default tierSlice.reducer;
