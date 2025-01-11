819c9f415abd340bd0085b46ef448f7a44f56e71
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 05:53:56 +0200

    components<>used stylesheets in Welcome Bar

--- components/tabs/home/WelcomeBar.tsx
+++ components/tabs/home/WelcomeBar.tsx
@@ -1,36 +1,31 @@
-import { Image } from "react-native";
+import { Image, StyleSheet } from "react-native";
 import React from "react";
 import { useAppSelector } from "@/store/hooks";
 import CustomText from "@/components/base/CustomText";
 import CustomView from "@/components/base/CustomView";
 import { MaterialIcons } from "@expo/vector-icons";
 import { isValidProfilePictureUrl } from "@/lib/utils/authValidation.utils";
+import { colors } from "@/lib/constants/colors.constant";
+
 const WelcomeBar = () => {
 	const { userName, profilePictureUrl } = useAppSelector((state) => state.user);
 	const theme = useAppSelector((state) => state.system.colorScheme);
 
 	return (
-		<CustomView
-			style={{
-				width: "100%",
-				display: "flex",
-				flexDirection: "row",
-				justifyContent: "space-between",
-			}}
-		>
+		<CustomView style={styles.container}>
 			<CustomText
-				style={{
-					fontSize: 24,
-					fontFamily: "Raleway-Bold",
-				}}
+				style={styles.welcomeText}
 				content={`Welcome, ${userName ? userName : "John"}!`}
 			/>
 			{isValidProfilePictureUrl(profilePictureUrl) ? (
-				<Image source={{ uri: profilePictureUrl }} />
+				<Image
+					source={{ uri: profilePictureUrl }}
+					style={styles.profileImage}
+				/>
 			) : (
 				<MaterialIcons
 					name='face'
-					color={theme === "dark" ? "#fff" : "#000"}
+					color={theme === "dark" ? `${colors["white-font"]}` : `${colors["black-font"]}`}
 					size={30}
 				/>
 			)}
@@ -39,3 +34,21 @@ const WelcomeBar = () => {
 };
 
 export default WelcomeBar;
+
+const styles = StyleSheet.create({
+	container: {
+		width: "100%",
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+	welcomeText: {
+		fontSize: 24,
+		fontFamily: "Raleway-Bold",
+	},
+	profileImage: {
+		width: 30,
+		height: 30,
+		borderRadius: 15,
+	},
+});
