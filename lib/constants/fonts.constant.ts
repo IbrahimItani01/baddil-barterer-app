63a3b060af540bda9a5d93504666d591e11d382a
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 04:05:50 +0200

    constants<>created fonts constant to easily use custom fonts

---
+++ lib/constants/fonts.constant.ts
@@ -0,0 +1,18 @@
+export const fontFamily = {
+	Raleway: {
+		Light: "Raleway-Light",
+		Regular: "Raleway-Regular",
+		Medium: "Raleway-Medium",
+		SemiBold: "Raleway-SemiBold",
+		Bold: "Raleway-Bold",
+		ExtraBold: "Raleway-ExtraBold",
+	},
+	NunitoSans: {
+		Light: "NunitoSans-Light",
+		Regular: "NunitoSans-Regular",
+		Medium: "NunitoSans-Medium",
+		SemiBold: "NunitoSans-SemiBold",
+		Bold: "NunitoSans-Bold",
+		ExtraBold: "NunitoSans-ExtraBold",
+	},
+};
