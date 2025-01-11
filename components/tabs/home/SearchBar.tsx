c87a5dea687669ebffacecc347bdae92879d4a36
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 04:03:52 +0200

    components<>handled logic of search bar component

--- components/tabs/home/SearchBar.tsx
+++ components/tabs/home/SearchBar.tsx
@@ -4,6 +4,21 @@ import { colors } from "@/lib/constants/colors.constant";
 import { Searchbar } from "react-native-paper";
 
 const SearchBar = () => {
+	const [searchValue, setSearchValue] = useState("");
+	const [searching, setSearching] = useState(false);
+	const handleChange = (newValue: string) => setSearchValue(newValue);
+	const handleClear = () => {
+		setSearchValue("");
+		setSearching(false);
+	};
+
+	const handleSearch = () => {
+		setSearching(true);
+		setTimeout(() => {
+			setSearching(false);
+			setSearchValue("");
+		}, 2000);
+	};
 };
 
 export default SearchBar;
