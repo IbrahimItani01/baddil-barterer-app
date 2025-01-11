0ebb701b9f984d4116a5b6649f83560d673e9db3
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 04:11:43 +0200

    components<>handled tier progress state

--- components/tabs/home/TierProgressBar.tsx
+++ components/tabs/home/TierProgressBar.tsx
@@ -1,7 +1,14 @@
-import { View, Text } from "react-native";
+import { useColorScheme, View } from "react-native";
 import React from "react";
+import CustomText from "@/components/base/CustomText";
+import { useAppSelector } from "@/store/hooks";
+import CustomView from "@/components/base/CustomView";
+import * as Progress from "react-native-progress";
+import { colors } from "@/lib/constants/colors.constant";
 
 const TierProgressBar = () => {
+	const { progress, currentTier } = useAppSelector((state) => state.tiers);
+	const theme = useColorScheme();
 	return (
 		<View>
 			<Text>TierProgressBar</Text>
