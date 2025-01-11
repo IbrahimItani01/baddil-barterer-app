35dc49f0775312508aaa82a6083f70017ed12ea1
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 08:19:31 +0200

    components<>handled logic and animation of AiFAB

--- components/base/AiFAB.tsx
+++ components/base/AiFAB.tsx
@@ -1,7 +1,32 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+	View,
+	TouchableOpacity,
+	StyleSheet,
+	Animated,
+	Easing,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { colors } from "@/lib/constants/colors.constant";
+import { MaterialIcons } from "@expo/vector-icons";
+import AiFABMenu from "./AiFABMenu";
 
 const AiFAB = () => {
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [fadeAnim] = useState(new Animated.Value(0));
+
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	};
+
+	useEffect(() => {
+		Animated.timing(fadeAnim, {
+			toValue: menuOpen ? 1 : 0,
+			duration: 200,
+			useNativeDriver: true,
+			easing: Easing.ease,
+		}).start();
+	}, [menuOpen]);
+
 	return (
 		<View>
 			<Text>AiFAB</Text>
