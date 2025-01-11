b304e4f143dbf89744074ede54d35bb27f226c03
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 04:03:34 +0200

    components<>created SearchBar component to include in home screen

---
+++ components/tabs/home/SearchBar.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+import React, { useState } from "react";
+import { colors } from "@/lib/constants/colors.constant";
+import { Searchbar } from "react-native-paper";
+
+const SearchBar = () => {
+};
+
+export default SearchBar;
