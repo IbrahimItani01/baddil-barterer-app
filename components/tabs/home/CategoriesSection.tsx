9dc16e00a88030e1d30d1d724349cc58495fb5f8
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 04:14:03 +0200

    components<>displayed categoryButtons using flat list

--- components/tabs/home/CategoriesSection.tsx
+++ components/tabs/home/CategoriesSection.tsx
@@ -1,10 +1,25 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+import { colors } from "@/lib/constants/colors.constant";
+import { categories } from "@/lib/constants/categories.constant";
+import { fontFamily } from "@/lib/constants/fonts.constant";
+import CategoryButton from "./base/CategoryButton";
+import CustomText from "@/components/base/CustomText";
 
 const CategoriesSection = () => {
 	return (
-		<View>
-			<Text>CategoriesSection</Text>
+		<View style={styles.container}>
+			<CustomText
+				content='Categories'
+				style={styles.title}
+			/>
+			<FlatList
+				data={categories.slice(0, 8)}
+				numColumns={4}
+				keyExtractor={(item) => item.id.toString()}
+				renderItem={({ item }) => <CategoryButton item={item} />}
+			/>
 		</View>
 	);
 };
