97914def9515d6058d7b45e74d14e289694fd694
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 04:13:08 +0200

    components<>styled category button component

--- components/tabs/home/base/CategoryButton.tsx
+++ components/tabs/home/base/CategoryButton.tsx
@@ -10,6 +10,42 @@ interface CategoryButtonProps {
 }
 
 const CategoryButton: React.FC<CategoryButtonProps> = ({ item }) => {
+	return (
+		<TouchableOpacity style={styles.categoryContainer}>
+			<View style={styles.iconWrapper}>
+				<FontAwesome5
+					name={item.iconName}
+					size={24}
+					color='#fff'
+				/>
+			</View>
+			<CustomText
+				content={item.name}
+				style={styles.categoryText}
+			/>
+		</TouchableOpacity>
+	);
 };
 
 export default CategoryButton;
+
+const styles = StyleSheet.create({
+	categoryContainer: {
+		flex: 1,
+		alignItems: "center",
+		marginBottom: 12,
+	},
+	iconWrapper: {
+		width: 60,
+		height: 60,
+		borderRadius: 12,
+		backgroundColor: colors.primary,
+		alignItems: "center",
+		justifyContent: "center",
+		marginBottom: 8,
+	},
+	categoryText: {
+		fontSize: 12,
+		textAlign: "center",
+	},
+});
