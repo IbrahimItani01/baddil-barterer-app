892fa215a4be585197675e1b9b4ae5214b31e1e3
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-11 04:05:25 +0200

    constants<>added new colors in colors constant

--- lib/constants/colors.constant.ts
+++ lib/constants/colors.constant.ts
@@ -1,7 +1,9 @@
 export const colors = {
 	primary: "#E60000",
-	"light-bg": "#FFFFFF", 
-	"dark-bg": "#140000", 
-	"black-font": "#000000", 
-	"white-font": "#FFFFFF", 
+	"light-bg": "#FFFFFF",
+	"dark-bg": "#140000",
+	"black-font": "#000000",
+	"white-font": "#FFFFFF",
+	"light-gray-light-theme": "#D3D3D3",
+	"dark-gray-dark-theme": "#4A4A4A",
 };
