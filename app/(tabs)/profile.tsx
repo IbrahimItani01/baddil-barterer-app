a0032ce050542f7f515706b97ed7181d575e21bf
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-14 03:40:04 +0200

    components<>finalized profile page main component

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,17 @@
-import CustomText from "@/components/base/CustomText";
-import CustomView from "@/components/base/CustomView";
-import '../../global.css'
+import React from "react";
+import TabScreen from "@/components/tabs/base/TabScreen";
+import ProfileHeader from "@/components/tabs/profile/ProfileHeader";
+import ProfileSection from "@/components/tabs/profile/ProfileSection";
+import SettingsSection from "@/components/tabs/profile/SettingsSection";
 
-export default function Tab() {
+export default function ProfileScreen() {
 	return (
-			<CustomView NativeClasses='h-full flex justify-center items-center'>
-				<CustomText content='profile' />
-			</CustomView>
+		<TabScreen title='Profile'>
+			<ProfileHeader />
+
+			<ProfileSection />
+
+			<SettingsSection />
+		</TabScreen>
 	);
 }
