4c4f8c24f1da2305fd142077c8a0f7486d8294f9
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-15 01:58:40 +0200

    utils<>declared replace local host for easy switch of links

--- lib/utils/general.utils.ts
+++ lib/utils/general.utils.ts
@@ -7,3 +7,7 @@ export const capitalizeFirstLetter = (string: string) => {
 export const formatDate = (date: string) => {
 	return dayjs(date).format("MMMM D, YYYY");
 };
+export const  replaceLocalhost = (url: string, newAddress: string): string=> {
+	return url.replace(/^http:\/\/localhost/, `http://${newAddress}`);
+  }
+  
