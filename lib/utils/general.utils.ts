48bc5c90d336ab1bbf021e21cb49d0df3976c950
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-14 18:20:17 +0200

    utils<>created a function to format date

--- lib/utils/general.utils.ts
+++ lib/utils/general.utils.ts
@@ -1,3 +1,9 @@
+import dayjs from "dayjs";
+
 export const capitalizeFirstLetter = (string: string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-  };
+	return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+
+export const formatDate = (date: string) => {
+	return dayjs(date).format("MMMM D, YYYY");
+};
