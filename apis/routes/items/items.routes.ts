78499215460f61a4a5133bbf48e0ef899238534e
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-15 00:07:08 +0200

    routes<>declared fetchUserItems function

--- apis/routes/items/items.routes.ts
+++ apis/routes/items/items.routes.ts
@@ -35,3 +35,26 @@ export const fetchNonUserItems = async () => {
 	}
 };
 
+export const fetchUserItems = async (userId?: string) => {
+	try {
+		const token = await AsyncStorage.getItem("jwtToken");
+		const endpoint = userId
+			? `${APIS_BASE_URL}/wallet/items/user/${userId}`
+			: `${APIS_BASE_URL}/wallet/items/user`;
+
+		const response = await axios.get(endpoint, {
+			headers: {
+				Authorization: `Bearer ${token}`, // Include JWT from local storage or state
+			},
+		});
+
+		if (response.data.success) {
+			return response.data.data;
+		} else {
+			throw new Error(response.data.message || "Failed to fetch user items");
+		}
+	} catch (error) {
+		console.error("Error fetching user wallet items:", error);
+		throw error; // Propagate error for handling in the calling function
+	}
+};
