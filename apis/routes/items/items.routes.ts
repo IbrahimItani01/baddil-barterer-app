0e1951bb02da09cc7b7fe58a65599591a56521fe
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-15 00:06:47 +0200

    routes<>declared fetchNonUserItems function

--- apis/routes/items/items.routes.ts
+++ apis/routes/items/items.routes.ts
@@ -1,1 +1,37 @@
-// TODO: fetch items apis
+import { APIS_BASE_URL } from "@/apis/main";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+interface AddItemToWalletPayload {
+	name: string;
+	description: string;
+	categoryId: string;
+	subcategoryId: string;
+	condition: string;
+	locationId: string;
+	files: File[]; // Files to upload (exactly 5 images)
+}
+
+
+export const fetchNonUserItems = async () => {
+	try {
+		const token = await AsyncStorage.getItem("jwtToken");
+		const response = await axios.get(
+			`${APIS_BASE_URL}/wallet/items/not-owned`,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`, // Include JWT from local storage or state
+				},
+			}
+		);
+
+		if (response.data.success) {
+			return response.data.data;
+		} else {
+			throw new Error(response.data.message || "Failed to fetch items");
+		}
+	} catch (error) {
+		console.error("Error fetching items not owned by the user:", error);
+		throw error; // Propagate error for handling in the calling function
+	}
+};
+
