333f7a188349f1532d2cc34dc91ee2e5b7eaaa36
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-15 00:13:25 +0200

    constants<>fixed mock data categories

--- lib/constants/categories.constant.ts
+++ lib/constants/categories.constant.ts
@@ -1,179 +1,121 @@
 export const categories = [
-    {
-      id: 1,
-      name: "vehicles",
-      iconName: "car",
-      subcategories: [
-        {
-          id: 2,
-          name: "cars",
-          iconName: "car",
-        },
-        {
-          id: 3,
-          name: "motorcycles",
-          iconName: "motorcycle",
-        },
-        {
-          id: 4,
-          name: "trucks",
-          iconName: "truck",
-        },
-        {
-          id: 5,
-          name: "bicycles",
-          iconName: "bicycle",
-        },
-      ],
-    },
-    {
-      id: 6,
-      name: "electronics",
-      iconName: "tablet-alt",
-      subcategories: [
-        {
-          id: 7,
-          name: "phones",
-          iconName: "mobile-alt",
-        },
-        {
-          id: 8,
-          name: "laptops",
-          iconName: "laptop",
-        },
-        {
-          id: 9,
-          name: "televisions",
-          iconName: "tv",
-        },
-        {
-          id: 10,
-          name: "cameras",
-          iconName: "camera",
-        },
-      ],
-    },
-    {
-      id: 11,
-      name: "furniture",
-      iconName: "couch",
-      subcategories: [
-        {
-          id: 12,
-          name: "sofas",
-          iconName: "couch",
-        },
-        {
-          id: 13,
-          name: "tables",
-          iconName: "table",
-        },
-        {
-          id: 14,
-          name: "beds",
-          iconName: "bed",
-        },
-      ],
-    },
-    {
-      id: 15,
-      name: "instruments",
-      iconName: "music",
-      subcategories: [
-        {
-          id: 16,
-          name: "guitars",
-          iconName: "guitar",
-        },
-        {
-          id: 17,
-          name: "keyboards",
-          iconName: "keyboard",
-        },
-        {
-          id: 18,
-          name: "drums",
-          iconName: "drum",
-        },
-      ],
-    },
-    {
-      id: 19,
-      name: "fashion",
-      iconName: "tshirt",
-      subcategories: [
-        {
-          id: 20,
-          name: "clothing",
-          iconName: "shopping-bag",
-        },
-        {
-          id: 21,
-          name: "shoes",
-          iconName: "shoe-prints",
-        },
-        {
-          id: 22,
-          name: "accessories",
-          iconName: "watch",
-        },
-      ],
-    },
-    {
-      id: 23,
-      name: "books",
-      iconName: "book",
-      subcategories: [],
-    },
-    {
-      id: 24,
-      name: "sports",
-      iconName: "futbol",
-      subcategories: [
-        {
-          id: 25,
-          name: "gym equipment",
-          iconName: "dumbbell",
-        },
-        {
-          id: 26,
-          name: "bicycles",
-          iconName: "bicycle",
-        },
-        {
-          id: 27,
-          name: "outdoor gear",
-          iconName: "campground",
-        },
-      ],
-    },
-    {
-      id: 28,
-      name: "toys",
-      iconName: "puzzle-piece",
-      subcategories: [],
-    },
-    {
-      id: 29,
-      name: "appliances",
-      iconName: "blender",
-      subcategories: [
-        {
-          id: 30,
-          name: "fridges",
-          iconName: "snowflake",
-        },
-        {
-          id: 31,
-          name: "washing machines",
-          iconName: "soap",
-        },
-        {
-          id: 32,
-          name: "microwaves",
-          iconName: "hotdog",
-        },
-      ],
-    },
-  ];
-  
+	{
+		id: "1d981fe2-0203-49dc-8336-644a0aa5bfec",
+		name: "vehicles",
+		iconName: "car",
+		subcategories: [
+			{
+				id: "0fb982b2-4ea0-44ba-af1c-7a3b81f5c96b",
+				name: "cars",
+				iconName: "car",
+			},
+			{
+				id: "38622bf6-6259-4abe-a746-eaf2a406dbac",
+				name: "motorcycles",
+				iconName: "motorcycle",
+			},
+			{
+				id: "439b5f4e-31ff-4f09-931e-b4d971030a1d",
+				name: "trucks",
+				iconName: "truck",
+			},
+			{
+				id: "597d4e93-6afb-47b8-9c5e-fe3e8315ce8d",
+				name: "bicycles",
+				iconName: "bicycle",
+			},
+		],
+	},
+	{
+		id: "99794f9a-593d-4213-b3c6-0ae1213a57be",
+		name: "electronics",
+		iconName: "tablet-alt",
+		subcategories: [
+			{
+				id: "6117a226-6048-494d-9521-770f34e29434",
+				name: "phones",
+				iconName: "mobile-alt",
+			},
+			{
+				id: "8f8f09a8-258d-44ff-95fa-e66eec5c76ab",
+				name: "laptops",
+				iconName: "laptop",
+			},
+			{
+				id: "a74107c2-a715-45cf-8e5e-27d6b3d7f194",
+				name: "televisions",
+				iconName: "tv",
+			},
+			{
+				id: "c33513c4-099f-432f-8f59-9b5657a1bafe",
+				name: "cameras",
+				iconName: "camera",
+			},
+		],
+	},
+	{
+		id: "ac3cd0cb-f470-4c47-adfb-bad1c0ae3266",
+		name: "furniture",
+		iconName: "couch",
+		subcategories: [],
+	},
+	{
+		id: "b7442be3-0cb8-4553-99d8-8145406bd0f2",
+		name: "instruments",
+		iconName: "music",
+		subcategories: [],
+	},
+	{
+		id: "c3d218b9-3ba2-4fa9-a4ba-f19a9d78e1aa",
+		name: "fashion",
+		iconName: "tshirt",
+		subcategories: [
+			{
+				id: "da2487e5-1d3a-47b8-a8a5-e9d7a50c5639",
+				name: "clothing",
+				iconName: "shopping-bag",
+			},
+			{
+				id: "dfc78b50-a1b0-458d-bbc6-27a2bed7808c",
+				name: "shoes",
+				iconName: "shoe-prints",
+			},
+		],
+	},
+	{
+		id: "83e17400-91b3-4485-985e-74aa9dd33d33",
+		name: "books",
+		iconName: "book",
+		subcategories: [],
+	},
+	{
+		id: "214ccff2-1ec5-4fd0-8b4b-f0de9ae4ceee",
+		name: "sports",
+		iconName: "futbol",
+		subcategories: [
+			{
+				id: "b361ed5f-2089-44d7-8aad-9949295f7ead",
+				name: "bicycles",
+				iconName: "bicycle",
+			},
+			{
+				id: "4e71d8c0-17e9-44b6-9aeb-c6078c0a7694",
+				name: "other",
+				iconName: "campground",
+			},
+		],
+	},
+	{
+		id: "48412c6a-650c-4422-93df-c078e8914819",
+		name: "toys",
+		iconName: "puzzle-piece",
+		subcategories: [],
+	},
+	{
+		id: "db53d014-aca8-4e45-9516-6f9dbaeb36cc",
+		name: "appliances",
+		iconName: "blender",
+		subcategories: [
+		],
+	},
+];
