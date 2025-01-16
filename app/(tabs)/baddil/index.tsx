205cb66e9c5bf78adfb2dc56a5df59a3f378285d
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-16 21:09:28 +0200

    index<>fixed type issue

--- app/(tabs)/baddil/index.tsx
+++ app/(tabs)/baddil/index.tsx
@@ -1,28 +1,29 @@
 import CustomText from "@/components/base/CustomText";
 import TabScreen from "@/components/tabs/base/TabScreen";
-import { categories } from "@/lib/constants/categories.constant";
 import CategoryCard from "@/components/tabs/baddil/CategoryCard";
 import { FlatList } from "react-native";
 import { fontFamily } from "@/lib/constants/fonts.constant";
 import { useRouter } from "expo-router";
+import { useAppSelector } from "@/store/hooks"; // Import the selector hook
+import { Subcategory } from "@/store/slices/categories.slice";
 
 export default function Tab() {
 	const router = useRouter();
 
+	// Access categories from Redux state
+	const categories = useAppSelector((state) => state.categories.categories);
+
 	const handleCategoryPress = (
 		categoryId: string,
-		subcategories: {
-			id: number;
-			name: string;
-			iconName: string;
-		}[]
+		subcategories: Subcategory[]
 	) => {
-		if (subcategories.length>0) {
+		if (subcategories.length > 0) {
 			router.push(`/(tabs)/baddil/(subPages)/Subcategories?id=${categoryId}`);
 		} else {
 			router.push(`/(tabs)/baddil/(details)/DetailsPage?id=${categoryId}`);
 		}
 	};
+
 	return (
 		<TabScreen title='Baddil'>
 			<CustomText
@@ -35,13 +36,13 @@ export default function Tab() {
 				data={categories}
 				style={{
 					height: "85%",
-					marginTop:5
+					marginTop: 5,
 				}}
 				keyExtractor={(item) => item.id.toString()}
 				renderItem={({ item }) => (
 					<CategoryCard
 						name={item.name}
-						iconName={item.iconName}
+						iconName={item.category_icon}
 						subcategories={item.subcategories}
 						onPress={() =>
 							handleCategoryPress(item.id.toString(), item.subcategories)
