63c5b0d48efb0364c3d04f2b62a260106dd73d48
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-16 21:08:43 +0200

    routes<>renamed get all location function for name conflict

--- apis/main.ts
+++ apis/main.ts
@@ -1,3 +1,3 @@
 // TODO: hub to handle all apis calls
-export const currentIp = '200.200.200.105'
+export const currentIp = '192.168.44.103'
 export const APIS_BASE_URL = `http://${currentIp}:8800/api`;
--- apis/routes/locations/locations.routes.ts
+++ apis/routes/locations/locations.routes.ts
@@ -1,7 +1,7 @@
 import { APIS_BASE_URL } from "@/apis/main"
 import axios from "axios"
 
-export const getAllLocations = async ()=>{
+export const getAllLocationsData = async ()=>{
     const response = await axios.get(`${APIS_BASE_URL}/locations`)
     if (response.data.success){
         return response.data.data
