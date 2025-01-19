42032e1a8f20fbffa5db3d04609537803894ea0d
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-20 00:42:56 +0200

    constants<>cleaned code

--- lib/constants/connection.constant.ts
+++ lib/constants/connection.constant.ts
@@ -1,6 +1,6 @@
 import * as Network from "expo-network";
 
 export const getBaseAppUrl = async () => {
-  const ipAddress = await Network.getIpAddressAsync();
-  return `http://${ipAddress}:8081`;
+	const ipAddress = await Network.getIpAddressAsync();
+	return `http://${ipAddress}:8081`;
 };
